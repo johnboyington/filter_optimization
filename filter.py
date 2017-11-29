fd86599a82295fdfa6f75c31c3ae7c8b4c02f45a
John Boyington <mjcb@ksu.edu>
2017-11-29 16:46:32 -0600

    produce and extract gamma data

--- filter.py
+++ filter.py
@@ -55,8 +55,42 @@ class Filter(object):
         with open('{}n.i'.format(self.ID), 'w+') as F:
             F.write(s)
 
+    def write_gamma(self):
+        default_text = open('gamma_template.i', 'r').read().split('*FLAG*')
+        s = default_text[0]
+        for d in range(self.num_disc):
+            m = self.material_bank[self.materials[d]]
+            args = [200 + d, m.num, -m.density, 130 + d, 131 + d, m.name]
+            s += '{}  {} {:9.5f} 120 -121 122 -123  {} -{}   $ {}\n'.format(*args)
+        s += default_text[1]
+        s += '90 0 -32 #11 #12 #13 (-130:-120:121:-122:123:{}) #31\n'.format(130+self.num_disc)
+        s += default_text[2]
+        for d in range(self.num_disc):
+            args = [131 + d, self.length * ((d + 1) / self.num_disc)]
+            s += '{} PX  {:9.5f}\n'.format(*args)
+        s += '41  PX  {:9.5f}     $ Detector\n'.format(self.length + 0.0002)
+        s += default_text[3]
+        s += 'IMP:p 1 {}r 0\n'.format(self.num_disc + 4)
+        s += default_text[4]
+        for m in set(self.materials + [0, 1, 2, 3]):
+            mat = self.material_bank[m]
+            s += 'c  -----------------------------------------------------------------------------\n'
+            s += 'c  MATERIAL  {:3d}: {}\n'.format(mat.num, mat.name)
+            s += 'c  -------------------------------------(density {:8.5} g/cm^3)---------------\n'.format(mat.density)
+            for i, comp in enumerate(mat.composition):
+                if i == 0:
+                    card = 'M{}'.format(mat.num)
+                else:
+                    card = '    '
+                s += '{}      {}   {}\n'.format(card, comp[0], comp[1])
+            s += 'c\n'
+        s += default_text[5]
+        with open('{}g.i'.format(self.ID), 'w+') as F:
+            F.write(s)
+
     def write(self):
         self.write_neutron()
+        self.write_gamma()
 
     def extract_neutron(self):
         with open('{}n.io'.format(self.ID), 'r') as f:
@@ -82,13 +116,43 @@ class Filter(object):
         self.n_g = d[21]['value'][4] + d[21]['value'][5]
         self.fast_to_total = d[11]['value'][5] / self.n_tot
 
+    def extract_gamma(self):
+        with open('{}g.io'.format(self.ID), 'r') as f:
+            mcnpOutput = f.read()
+        s = r'1tally'
+        tallyLocator = re.compile(s)
+        tallyIndx = tallyLocator.finditer(mcnpOutput)
+        indx = []
+        for match in tallyIndx:
+            indx.append(match.span()[1])
+        s = r'\d.\d\d\d\dE[+-]\d\d   \d.\d\d\d\d\dE[+-]\d\d \d.\d\d\d\d'
+        dataPtrn = re.compile(s)
+        keys = [21]
+        d = {keys[0]: {'energy': [], 'value': [], 'sigma': []}}
+        for i in range(len(keys)):
+            data = dataPtrn.finditer(mcnpOutput[indx[i]:indx[i + 1]])
+            for datum in data:
+                dataStr = datum.group().split()
+                d[keys[i]]['energy'].append(float(dataStr[0]))
+                d[keys[i]]['value'].append(float(dataStr[1]))
+                d[keys[i]]['sigma'].append(float(dataStr[2]))
+        self.g_g = d[21]['value'][4] + d[21]['value'][5]
+
+    def extract(self):
+        self.extract_neutron()
+        os.system('rm {}n.i*'.format(self.ID))
+        self.extract_gamma()
+        os.system('rm {}g.i*'.format(self.ID))
+        self.neutron_to_gamma = self.n_tot / (self.g_g + self.n_g)
+
     def run_local(self):
         os.system('mcnp6 name={}n.i'.format(self.ID))
+        os.system('mcnp6 name={}g.i'.format(self.ID))
 
 
 if __name__ == '__main__':
     c = [10, 1, 2, 3, 2, 2, 1, 2, 1, 2, 3, 1, 2, 3, 1, 2, 2, 1, 1, 1, 2, 2, 1, 2, 1, 2, 2, 1, 1, 2, 1]
     indiv = Filter(0, c)
-    indiv.write_neutron()
+    indiv.write()
     indiv.run_local()
-    indiv.extract_neutron()
+    indiv.extract()
