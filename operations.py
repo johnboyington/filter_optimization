12cf0fbc778942932f7df51c5a95b12b7376fdb4
John Boyington <mjcb@ksu.edu>
2017-11-29 22:39:20 -0600

    population runs with test; slicing and mutation operations added

---
+++ operations.py
@@ -0,0 +1,38 @@
+from filter import Filter
+from numpy.random import rand, randint
+from material import test_library
+
+
+class Operations(object):
+
+    def __init__(self):
+        self.set_params()
+
+    def set_params(self):
+        self.min_length = 3
+        self.max_length = 20
+        self.num_disc = 30
+        self.num_mat = len(test_library.lib)
+
+    def splice(self, child_ID, parent_1, parent_2):
+        assert isinstance(parent_1, Filter), 'splicing requires Filter objects'
+        assert isinstance(parent_2, Filter), 'splicing requires Filter objects'
+        child_chrom = []
+        for genome in range(len(parent_1.chromosome)):
+            if rand() > 0.5:
+                child_chrom += parent_1.chromosome[genome]
+            else:
+                child_chrom += parent_2.chromosome[genome]
+        return Filter(child_ID, child_chrom)
+
+    def mutate(self, ID, individual, gene):
+        assert isinstance(individual, Filter), 'mutating requires Filter objects'
+        if gene == 1:
+            new_gene = rand() * (self.max_length - self.min_length) + self.min_length
+        else:
+            new_gene = [randint(0, self.num_mat - 1)]
+        chrom = individual.chromosome
+        chrom[gene] = new_gene
+        return Filter(ID, chrom)
+
+ops = Operations()
--- population.py
+++ population.py
@@ -1,6 +1,7 @@
 from filter import Filter
 from numpy.random import rand, randint
 from material import test_library
+from operations import ops
 import matplotlib.pyplot as plt
 
 
@@ -8,26 +9,30 @@ class Population(object):
 
     def __init__(self, initial_pop):
         self.initial_population = initial_pop
+        self.set_params()
         self.current_generation = []
         self.historic_population = 0
+        self.next_generation = []
 
     def set_params(self):
         self.min_length = 3
-        self.max_length = 50
+        self.max_length = 20
         self.num_disc = 30
-        self.num_mat = len(test_library)
+        self.num_mat = len(test_library.lib)
+        self.next_gen_size = 10
+        self.children_per_crossover = 2
 
     def spawn_random(self, ID):
         chrom = []
         length = rand() * (self.max_length - self.min_length) + self.min_length
         chrom += [length]
-        for mat in range(self.num_mat):
+        for mat in range(self.num_disc):
             chrom += [randint(0, self.num_mat - 1)]
         print(chrom)
         return Filter(ID, chrom)
 
     def init_current_gen(self):
-        for i in self.initial_population:
+        for i in range(self.initial_population):
             self.current_generation += [self.spawn_random(self.historic_population)]
             self.historic_population += 1
 
@@ -36,16 +41,25 @@ class Population(object):
             if ind.fitness == -1:
                 ind.run_local()
 
+    def init_next_gen(self):
+        # TODO: finish this fucntion
+        for i in range(self.initial_population):
+            self.next_generation += [self.spawn_random(self.historic_population)]
+            self.historic_population += 1
+        return
+
     def plot_current_gen(self):
         ids = range(len(self.current_generation))
         fitnesses = []
         for i in ids:
             fitnesses.append(self.current_generation[i].fitness)
-        plt.plot(ids, fitnesses)
+        plt.figure(10)
+        plt.plot(ids, fitnesses, 'ko')
+        plt.show()
 
 
 if __name__ == '__main__':
-    test = Population(10)
+    test = Population(4)
     test.init_current_gen()
     test.run_current_gen()
     test.plot_current_gen()
