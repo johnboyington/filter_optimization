350ff44661def9e50f0a728c8c98c435a16a0248
John Boyington <mjcb@ksu.edu>
2017-11-30 17:27:36 -0600

    added test_cycle

---
+++ test_cycle.py
@@ -0,0 +1,33 @@
+from population import Population
+
+
+class Test_Cycle(object):
+
+    def __init__(self, num_gens):
+        self.num_generations = num_gens
+        self.iterate()
+        self.plot()
+
+    def iterate(self):
+        self.origin()
+        for i in range(len(self.num_generations)):
+            self.cycle()
+            self.plot()
+
+    def origin(self):
+        self.test = Population(20)
+        self.test.init_current_gen()
+        self.test.run_current_gen()
+        self.test.sort_current_gen()
+
+    def cycle(self):
+        self.test.init_next_gen()
+        self.test.run_current_gen()
+        self.test.sort_current_gen()
+
+    def plot(self):
+        self.test.plot_current_gen()
+
+
+if __name__ == '__main__':
+    test = Test_Cycle(10)
