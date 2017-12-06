96d8626b307e508b194bb7a4cae331a49a96ad1d
John Boyington <mjcb@ksu.edu>
2017-12-06 16:17:19 -0600

    ran single optimization

--- parameters.py
+++ parameters.py
@@ -4,12 +4,13 @@ from material import test_library
 class Parameters(object):
 
     def __init__(self):
-        self.start_size = 26
-        self.num_gens = 1
+        self.start_size = 30
+        self.num_gens = 2
         self.min_length = 3
         self.max_length = 20
         self.num_disc = 30
         self.num_mat = len(test_library.lib)
         self.next_gen_size = self.start_size
-        self.mut_per_gen = 5
+        self.mut_per_gen = int(self.next_gen_size * 0.05)
         self.max_population = self.start_size
+
